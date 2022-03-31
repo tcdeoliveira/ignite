cb6da303271bd564398415eb0676676c9bc03110
Tiago Oliveira <tiagoclaudio@outlook.com>
2022-03-31 21:55:57 +0100

    [Edit]_document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,24 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head>
+        <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
             crossOrigin="true"
           />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-
-          <link
-            href="https://fonts.googleapis.com/css2?family=Barlow:wght@500;600&display=swap"
-            rel="stylesheet"
-          />
         </Head>
-
+        <title>ig.news</title>
       <body>
         <Main />
         <NextScript />
