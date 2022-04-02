df3abe69a60ac06b9390b057a2c47f71d8bba904
Tiago Oliveira <tiagoclaudio@outlook.com>
2022-04-02 21:06:08 +0100

    [EDIT]Style header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,24 @@
 import styles from "./style.module.scss"
 import { SignInButton } from '../SignInButton/index';
+import Image from "next/image"
 export function Header() {
     return(
-        <header>
-            <h1>ig.news</h1>
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>Posts</li>
-                </ul>
-            </nav>
-            <SignInButton/>
+        <header className={styles.header}>
+            <div className={styles.header__container}>
+                <Image 
+                    width="108.45" 
+                    height="30.27" 
+                    src="/images/ig.news.svg" 
+                    alt="ig-news-logo" 
+                />   
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Posts</a>
+                    <ul>
+                    </ul>
+                </nav>
+                <SignInButton/>
+            </div>
         </header>
     );
 }
