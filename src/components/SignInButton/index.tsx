17b19fef6dfe488951f9759ccd4284b4c0371657
Tiago Oliveira <tiagoclaudio@outlook.com>
2022-04-03 00:56:56 +0100

    [CHANGE]Style SigInButton component

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,7 +1,18 @@
 import styles from "./style.module.scss"
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+
 export function SignInButton() {
-    return(
-        <button>
+    const isUserLoggedIn = true;
+    return isUserLoggedIn ? (
+        <button className={styles.signInButton} >
+            <FaGithub color="#04d361"/>
+            User Name
+            <FiX color="#737380" className={styles.closeIcon}/>
+        </button>
+    ) : (
+        <button className={styles.signInButton} >
+            <FaGithub color="#eba417"/>
             Sign with GitHub
         </button>
     );
