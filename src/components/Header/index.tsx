3e23898751b92c5ce9ca2d2de0209eb649c59bc1
Tiago Oliveira <tiagoclaudio@outlook.com>
2022-04-02 19:29:16 +0100

    [Add]Header, SignInButton components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,16 @@
+import styles from "./style.module.scss"
+import { SignInButton } from '../SignInButton/index';
 export function Header() {
     return(
         <header>
-            Header
+            <h1>ig.news</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Posts</li>
+                </ul>
+            </nav>
+            <SignInButton/>
         </header>
     );
 }
---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,8 @@
+import styles from "./style.module.scss"
+export function SignInButton() {
+    return(
+        <button>
+            Sign with GitHub
+        </button>
+    );
+}
