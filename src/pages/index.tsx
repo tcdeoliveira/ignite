865139383186248f86af076f46556102222af67a
Tiago Oliveira <tiagooliveira@takethewind.com>
2022-04-08 11:09:18 +0100

    [Change]Template home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import styles from "./home.module.scss"
+import { SubscribeButton } from '../components/SubscribeButton/index';
 
 export default function Home() {
   return (
@@ -7,7 +9,16 @@ export default function Home() {
       <Head>
         <title>ig.news | Home</title>
       </Head>
-      <h1>Home</h1>
+      <main className={styles.homeContainer}>
+        <section className={styles.hero}>
+          <span className={styles.homeContainer__heyWelcomeSpan}>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>Get access to all <br/> <span>for $9,90 month</span></p>
+          <SubscribeButton/>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
+
