719f32e119a3646ffd9706d7a742a78111355baf
Tiago Oliveira <tiagooliveira@takethewind.com>
2022-04-14 23:47:09 +0100

    [CHANGES]Back-end no front-end

---
+++ .slicemachine/assets/slices/Post/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Post';
+
+export default {
+  title: 'slices/Post'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"post","items":[],"primary":{"title":[{"type":"heading1","text":"Unleash leading-edge infrastructures","spans":[]}],"description":[{"type":"paragraph","text":"Esse occaecat aliqua aliquip officia aliqua Lorem fugiat.","spans":[]}],"content":[{"type":"paragraph","text":"Enim est fugiat aliqua excepteur incididunt est ipsum nostrud Lorem laboris veniam exercitation aliqua.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
