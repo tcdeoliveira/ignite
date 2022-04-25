4f70d4265b5b9eba4f12b16f9b408f961ae5987d
Tiago Oliveira <tiagooliveira@takethewind.com>
2022-04-25 14:41:42 +0100

    [CHANGE]ADD header inks

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import styles from "./style.module.scss"
 import { SignInButton } from '../SignInButton/index';
+import Link from "next/link";
 import Image from "next/image"
 export function Header() {
     return(
@@ -12,8 +13,12 @@ export function Header() {
                     alt="ig-news-logo" 
                 />   
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <Link href="/" prefetch>
+                        <a className={styles.active}>Home</a>
+                    </Link>
+                    <Link href="/posts" prefetch>
+                        <a>Posts</a>
+                    </Link>
                     <ul>
                     </ul>
                 </nav>
